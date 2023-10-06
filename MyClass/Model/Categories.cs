5603df5a52906931e7ed37a122d7a3a9f4198740
leminhthanh1010 <thanh.lm.63cntt@ntu.edu.vn>
2023-10-06 14:45:19 +0700

    init

--- MyClass/Model/Categories.cs
+++ MyClass/Model/Categories.cs
@@ -14,25 +14,36 @@ namespace MyClass.Model
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Tên loại sản phẩm không được để trống")]
+        [Display(Name="Tên loại hàng")]
         public string Name { get; set; }
+        [Display(Name = "Tên rút gọn")]
         public string Slug { get; set; }
+        [Display(Name = "Cấp cha")]
         public int? ParentId { get; set; }
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Mô tả không được để trống")]
+        [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Từ khóa không được để trống")]
+        [Display(Name = "Từ khóa")]
         public string MetaKey { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Người tạo không được để trống")]
+        [Display(Name = "Người tạo")]
         public int CreateBy { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Ngày tạo không được để trống")]
+        [Display(Name = "Ngày tạo")]
         public DateTime CreateAt { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Người cập nhật không được để trống")]
+        [Display(Name = "Người cập nhật")]
         public int UpdateBy { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Ngày cập nhật không được để trống")]
+        [Display(Name = "Ngày cập nhật")]
         public DateTime UpdateAt { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Trạng thái không được để trống")]
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
 
     }
